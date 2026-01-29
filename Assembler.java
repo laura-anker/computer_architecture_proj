e3c37de1ecbb896567fcb6832ca78585315dc597
laura-anker <lanker70@gwmail.gwu.edu>
2026-01-29 14:01:42 -0500

    added pass1 (untested) and source file example from instructions

--- Assembler.java
+++ Assembler.java
@@ -1,12 +1,16 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Assembler {
+    Map<String, Integer> dictionary = new HashMap<>();
+
     public void run(File sourceFile){
         if (pass1(sourceFile) == false){
-            //error!
+            return;//error!
         }
         if (pass2(sourceFile) == false){
             //error!
@@ -18,21 +22,40 @@ public class Assembler {
     //return true if successful, false if error
     public boolean pass1(File sourceFile){
         //set code location to 0
+        int codeLocation = 0;
         //Read a line of the file
         try (Scanner myReader = new Scanner(sourceFile)) { 
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                //Use the split command to break the line into its parts
-                //Process the line, if it is a label, add the label to a dictionary with the code location. Process
-                //the rest of the line (it could be blank, if so no code is generated). Check for errors in the
-                //code.
-                //If code or data was not generated, end loop. Otherwise increment code location
+                if (data.isEmpty() || data.startsWith(";")){//if line is empty or comment, move to next line
+                    continue;
+                }
+                //Use the split command to break the line into up to 4 parts based on one or more spaces
+                String[] splitData = data.data.trim().split("\\s+");
+
+                int val = 0;
+                if (splitData[0].endsWith(":")){//indicates label, used to define location
+                    String label = splitData[0].substring(0, splitData[0].length-1);//get label without :
+                    if dictionary.contains(label){//error if label is duplicated
+                        System.out.println("Duplicate label: "+label);
+                        return false;
+                    }
+                    dictionary.put(label, codeLocation);//add labal, location pair to dictionary
+                    val = 1;
+                }
+
+                //check for code or data generated after label (don't increment if there's not)
+                if (val< splitData.length()){
+                    codeLocation+=1; //increment location if code is generated
+                }
+                
             }
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
             return false;
         }
+        return true;
     }
 
     //use label/address map to generate machine code
